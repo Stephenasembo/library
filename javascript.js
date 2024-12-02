c117e7894de409af1861e364969d4eb9f122785b
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-02 21:42:50 +0300

    Iterate Over Every Book In Library

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ const library = [];
 
 function addBookToLibrary () {
     library.push(harry)
+    library.push(winnie)
 }
 
 function Book (title, author, pages, read) {
@@ -12,12 +13,16 @@ function Book (title, author, pages, read) {
 };
 
 let harry = new Book ('Harry Porter', 'Harry', 295, 'not read')
+let winnie = new Book ('Winnie the Poo', 'Nick', 100, 'read')
+addBookToLibrary()
 
 function displayBook () {
-    for (let book in library) {
-
+    for (let book of library) {
+        console.log(book.title);
+        console.log(book.author);
+        console.log(book.pages);
+        console.log(book.read);
     }
 }
 
-addBookToLibrary()
-console.log(library);
+displayBook()
