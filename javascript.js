8455b28778e65a43a0fcbd2da9f4b6556d5e81a5
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-03 10:00:24 +0300

    Create And Display New Book Added
    The library gets updated by the user

--- javascript.js
+++ javascript.js
@@ -13,9 +13,8 @@ const noRadio = document.querySelector('#no');
 
 const library = [];
 
-function addBookToLibrary () {
-    library.push(harry)
-    library.push(winnie)
+function addBookToLibrary (book) {
+    library.push(book);
 }
 
 function Book (title, author, pages, read) {
@@ -25,50 +24,39 @@ function Book (title, author, pages, read) {
     this.read = read
 };
 
-let harry = new Book ('Harry Porter', 'Harry', 295, 'not read')
-let winnie = new Book ('Winnie the Poo', 'Nick', 100, 'read')
-addBookToLibrary()
+let harry = new Book ('Harry Porter', 'Harry', 295, 'not read');
+let winnie = new Book ('Winnie the Poo', 'Nick', 100, 'read');
+addBookToLibrary(harry);
+addBookToLibrary(winnie);
 
-function displayBook () {
+function displayLibrary () {
     for (let book of library) {
-        const card = document.createElement('div');
-
-        const titlepara = document.createElement('p');
-        titlepara.innerText = book.title;
-        card.appendChild(titlepara);
-
-        const authorpara = document.createElement('p');
-        authorpara.innerText = book.author;
-        card.appendChild(authorpara);
-
-        const pagespara = document.createElement('p');
-        pagespara.innerText = book.pages;
-        card.appendChild(pagespara);
-
-        const readpara = document.createElement('p');
-        readpara.innerText = book.read;
-        card.appendChild(readpara);
-
-        output.appendChild(card);
+        displayNewBook(book);
     }
 }
 
-displayBook();
+displayLibrary();
 
 addNewBook.addEventListener('click', () => dialog.showModal());
 
 confirmBtn.addEventListener('click', (event) => {
     event.preventDefault();
     dialog.close()
-    console.log(bookTitle.value)
-    console.log(bookAuthor.value)
-    console.log(bookPages.value)
-
+    const newTitle = bookTitle.value;
+    const newAuthor = bookAuthor.value;
+    const newPages = bookPages.value;
     const readStatus = document.querySelector('input[type = "radio"]:checked');
+    let newRead;
+
     if (readStatus) {
-        console.log(readStatus.value);
+        newRead = readStatus.value;
     };
+
     readStatus.checked = false;
+
+    const newBook = new Book (newTitle, newAuthor, newPages, newRead);
+    addBookToLibrary(newBook);
+    displayNewBook(newBook);
 });
 
 cancelBtn.addEventListener('click', (event) => {
@@ -80,3 +68,25 @@ cancelBtn.addEventListener('click', (event) => {
     yesRadio.checked = false;
     noRadio.checked = false;
 });
+
+function displayNewBook (book) {
+    const card = document.createElement('div');
+
+    const titlepara = document.createElement('p');
+    titlepara.innerText = book.title;
+    card.appendChild(titlepara);
+
+    const authorpara = document.createElement('p');
+    authorpara.innerText = book.author;
+    card.appendChild(authorpara);
+
+    const pagespara = document.createElement('p');
+    pagespara.innerText = book.pages;
+    card.appendChild(pagespara);
+
+    const readpara = document.createElement('p');
+    readpara.innerText = book.read;
+    card.appendChild(readpara);
+
+    output.appendChild(card);
+}
